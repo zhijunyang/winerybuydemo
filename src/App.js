e202e8f848925bbeefd3ff9e7ec8e7f5b8cfbd17
zyang14 <zyang14@safeway.com>
2020-12-30 12:56:24 -0800

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PaypalButtons from "./components/PaypalButtons";
+import Car from "./assets/img/car.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    showPaypal: false
+  }; 
+
+  showPaypalButtons = () => {
+    this.setState({ showPaypal: true });
+  };
+
+  render() {
+    const { showPaypal } = this.state;
+    if (showPaypal) {
+      return <PaypalButtons />;
+    } else {
+      return (
+        <div className="main">
+          <h2> Buy this Mercedes at a giveaway price (Super Cheap) </h2>
+          <img alt="Mercedes G-Wagon" src={Car} />
+          <h3>
+            <b>$200</b>
+          </h3>
+          <button onClick={this.showPaypalButtons}> Pay </button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
